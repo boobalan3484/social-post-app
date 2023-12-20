ca2b363dcd5d30b9a7dbdf0ee328d840a7a80980
Boobalan <139740118+boobalan3484@users.noreply.github.com>
2023-12-20 11:28:35 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <main className='About'> 
+            <h2>About</h2>
+            <p>Hii, I'm Boobalan. I created a simple Social Media Post App</p>
+        </main>
+    )
+}
+
+export default About
